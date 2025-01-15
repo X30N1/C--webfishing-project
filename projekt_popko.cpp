67951dfd066e4a7ebc4f7f82abff64e8f2ac6c27
Xeon <53192187+X30N1@users.noreply.github.com>
2025-01-15 22:00:46 +0100

    Initial commit

--- projekt_popko.cpp
+++ projekt_popko.cpp
@@ -6,68 +6,183 @@
 using namespace std;
 
 class Fish {
-    private:
+    public:
         string name;
         int catchChance;
         int price;
-    public:
         Fish(string fishName, int chance, int pricing){
             name = fishName;
             catchChance = chance;
             price = pricing;
         }
+        Fish() = default;
 };
 
 class Rod {
-    private:
+    public:
         string name;
         float catchBonus;
         int price;
-    public:
-        Rod(string rodName, int bonus, int pricing){
+        Rod(string rodName, float bonus, int pricing){
             name = rodName;
             catchBonus = bonus;
             price = pricing;
         }
+        Rod() = default;
 
 };
 
 class Water {
-    private:
+    public:
         string name;
         vector<Fish> fishes;
-    public:
-        Water(vector<Fish> fishies, string waterName) {
+        int licenceCost;
+        Water(vector<Fish> fishies, string waterName, int licence) {
             name = waterName;
             fishes = fishies;
+            licenceCost = licence;
         }
+        string printName(){
+            return name;
+        }
+        Water() = default;
 };
 
 class FishingSys {
-    private:
+    public:
         Rod currentRod;
         Water currentWater;
         random_device rd;
+        vector<Water> places;
         vector<Rod> rods;
-        int currency;
-    public:
-        FishingSys(Rod starter, Water first){
+
+        vector<Fish> caughtfish;
+        int currency = 0;
+        FishingSys(Rod& starter, Water& first){
             currentRod = starter;
             currentWater = first;
-            rods.push_back(starter)
+            rods.push_back(starter);
         }
-        void setCurrentRod(Rod newrod){
+        void clearScreen(){
+            cout << string(100, '\n');
+        }
+        void setCurrentRod(Rod& newrod){
             currentRod = newrod;
         }
-        void setCurrentWater(Water newater) {
+        void setCurrentWater(Water& newater) {
             currentWater = newater;
         }
-        void printAllRods(){
-            cout << rods << endl;
-        }
         void printCurrency()
+        {
+            cout << "Masz obecnie " << currency << " Żetonów" << endl;
+        }
+        void pushRod(Rod& addrod){
+            rods.push_back(addrod);
+        }
+        void switchrod(){
+            clearScreen();
+            cout << "Wybierz wędkę: " << endl;
+            for (int i = 0; i < rods.size(); i++)
+            {
+                cout << i + 1 << ". " << rods[i].name << " | Bonus do wyłowienia: " << rods[i].catchBonus << " x" << endl;
+            }
+            int choice;
+            cin >> choice;
+            if (choice > rods.size() || choice < 1)
+            {
+                cout << "Nie ma takiej wędki" << endl;
+                switchrod();
+            }
+            else
+            {
+                setCurrentRod(rods[choice - 1]);
+                cout << "Zmieniono wędkę na: " << currentRod.name << endl;
+                overworldSelector();
+            }
+        }
+        void overworldSelector(){
+            clearScreen();
+            cout << "Obecnie jesteś w pobliżu: " << currentWater.printName() << endl;
+            cout << "Co chciałbyś / chciałabyś zrobić? \n1.Łów ryby \n2.Przejdź gdzie indziej \n3.Zmień ekwipunek \n4.Kup nowy ekwipunek \n5.Sprzedaj ryby \n";
+            int choice;
+            cin >> choice;
+            switch (choice)
+            {
+            case 1:
+                fishing();
+                break;
+            case 2:
+                //travel();
+                break;
+            case 3:
+                switchrod();
+                break;
+            case 4:
+                //buyEquipment();
+                break;
+            case 5:
+                //sellFish();
+                break;
+            default:
+                cout << "\nNie ma takiej opcji" << endl;
+                overworldSelector();
+                break;
+            }
+        }
 
+        void fishing()
+        {
+            clearScreen();
+            cout << "Zaczynasz łowić ryby" << endl;
+            int chance = currentWater.fishes[0].catchChance + (currentWater.fishes[0].catchChance * currentRod.catchBonus);
+            int random = rd() % 100;
+            cout << "Szansa złowienia ryby: " + to_string(chance) + "%" << endl;
+            cout << "Wylosowana liczba: " + to_string(random) << endl;
+
+
+            if (random < chance)
+            {
+                cout << "Udało Ci się złowić rybę!" << endl;
+                caughtfish.push_back(currentWater.fishes[0]);
+                overworldSelector();
+            }
+            else
+            {
+                cout << "Niestety nie udało Ci się złowić ryby" << endl;
+                overworldSelector();
+            }
+        }
+        void init(){
+            overworldSelector();
+        }
 };
+
+int main(){
+
+    // RYBY :D
+    Fish karp("Karp", 78,5);
+    Fish szczupak("Szczupak", 50, 10);
+    Fish leszcz("Leszcz", 60, 7);
+    Fish sandacz("Sandacz", 40, 12);
+    Fish pstrag("Pstrąg", 55, 9);
+    Fish sum("Sum", 30, 15);
+    Fish okon("Okoń", 70, 6);
+    Fish wegorz("Węgorz", 45, 11);
+    Fish karas("Karaś", 80, 4);
+    Fish lin("Lin", 65, 8);
+    Fish ploc("Płoć", 75, 5);
+
+    // Wędki
+    Rod start("Wędka startowa", 0, 0);
+
+    // Wody
+    Water jezioro({karp},"jeziorko", 0);
+
+    // Uruchamiamy skrypt
+    FishingSys system(start,jezioro);
+    system.init();
+    return 0;
+}
 
 
 
